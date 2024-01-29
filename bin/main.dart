f7041f5f15039cd29bec28942e126131e8496db9
osaxma <46427323+osaxma@users.noreply.github.com>
2024-01-30 02:35:57 +0300

    added some todos

--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,12 @@
 void main(List<String> args) {
+  // TODO: dart_types  <file_path> [args]
+  //
+  //      generate a type lattice between for class:
+  //      e.g. dart_types /path/to/file.dart --class="SomeWidget"
+  //
+  //      generate a type lattice between two classes:
+  //      e.g. dart_types /path/to/file.dart --class="SomeWidget" --class="AnotherWidget"
+  //
   print('TODO: CLI');
   return;
 }
